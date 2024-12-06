21f3e51c426ce3bb154a28a685d6f2b1c012e061
elliotabrahamsson <elliotsson@outlook.com>
2024-12-06 14:31:10 +0100

    changed back the url

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:10000/MrOlympias")
+fetch("https://json-server-7x9n.onrender.com/MrOlympias")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Kunde inte hämta från data.json" + response.statusText);
